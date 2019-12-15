a2df31df9565fcbf420907e4f17e41f07eceb11f
haniR <hani.m11@hotmail.com>
2019-12-15 14:27:32 +0200

    add some food

---
+++ Assets/timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float starttime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        starttime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log((Time.time - starttime).ToString("00:00.00"));
+
+    }
+}
